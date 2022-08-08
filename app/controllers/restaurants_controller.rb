7cc5f8a53e5cc30b311f6518404ed093c90d234a
Eduarda Campos <eduarda.prc@gmail.com>
2022-08-08 15:04:20 -0300

    Changed layout of views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,7 +11,7 @@ class RestaurantsController < ApplicationController
     if @restaurant.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render :edit
     end
   end
 
@@ -35,7 +35,7 @@ class RestaurantsController < ApplicationController
     if @restaurant.update(restaurant_params)
       redirect_to restaurant_path(@restaurant)
     else
-      render :edit
+      render :new
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews, only: %i[create]
   end
+
+  root to: 'restaurants#index'
 end
